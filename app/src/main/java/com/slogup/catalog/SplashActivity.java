c37c76d607a1875e3ddfb4374cb8552ff1fba7f3
YoungJin <ac12bd@slogup.com>
2016-08-02 16:52:41 +0900

    권한 체크 동의해야 넘어가게

--- app/src/main/java/com/slogup/catalog/SplashActivity.java
+++ app/src/main/java/com/slogup/catalog/SplashActivity.java
@@ -3,6 +3,7 @@ package com.slogup.catalog;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +26,7 @@ import org.json.JSONObject;
 public class SplashActivity extends AppCompatActivity {
     private static final String LOG_TAG = SplashActivity.class.getSimpleName();
 
+    private int CAMERA_PERMISSION_REQUEST = 1;
     private Context _this = this;
     private ImageView splashImageView;
 
@@ -35,14 +37,13 @@ public class SplashActivity extends AppCompatActivity {
 
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        splashImageView = (ImageView) findViewById(R.id.splashImageView);
+//        splashImageView = (ImageView) findViewById(R.id.splashImageView);
 //        Picasso.with(_this).load(R.drawable.icon).into(splashImageView);
 
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                0);
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, CAMERA_PERMISSION_REQUEST);
 
-        getProductData();
     }
 
 //    public void appInit() {
@@ -136,4 +137,21 @@ public class SplashActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    getProductData();
+
+                } else {
+
+                    finish();
+                }
+
+                return;
+            }
+        }
+    }
 }
